42eaed8aeceaa09c735a562aaecb3e15c7a42d1c
groibs <lucasds50@gmail.com>
2026-08-04 13:38:45 -0300

    feat(ui): export ProgressSteps

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -19,6 +19,13 @@ export type { SkeletonProps } from "./Skeleton";
 export { TranscribeBar } from "./TranscribeBar";
 export type { TranscribeBarProps } from "./TranscribeBar";
 
+export { ProgressSteps } from "./ProgressSteps";
+export type {
+  ProgressStep,
+  ProgressStepsProps,
+  ProgressStepsStatus,
+} from "./ProgressSteps";
+
 export { Logomark } from "./Logomark";
 
 export { cx } from "./cx";
